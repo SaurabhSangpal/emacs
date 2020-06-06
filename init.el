94eded09dc5fe8e87920755c65e32f7f1faeaab3
Saurabh Sangpal <saurabhsangpal@gmail.com>
2020-06-06 16:22:04 +0530

    Major update | Utilize use-package heavily

--- init.el
+++ init.el
@@ -1,238 +1,291 @@
-; Melpa
+;;; package -- summary
+;;; Commentary:
+;;; Initialization section
+
+;;; Melpa
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;; code:
 (package-initialize)
 
-(defun ensure-package-installed (&rest packages)
-  "Assure every package is installed, ask for installation if it’s not.
-
-Return a list of installed packages or nil for every skipped package."
-
+;;; Install use-package
 (unless package-archive-contents
   (package-refresh-contents))
+(if (package-installed-p 'use-package)
+    nil
+  (package-install 'use-package))
 
-  (mapcar
-   (lambda (package)
-     ;; (package-installed-p 'evil)
-     (if (package-installed-p package)
-         nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (package-install package)
-         package)))
-   packages))
+;;; Font
+(set-face-attribute 'default nil
+		    :family "JetBrains Mono"
+		    :height 120
+		    :weight 'regular
+		    :width 'condensed)
 
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-(ensure-package-installed 'magit 'csharp-mode 'flycheck 'helm 'dash 'f 'use-package 'async 'popup 'popwin 'company 'yasnippet 'omnisharp 'projectile 'smartparens 'powerline 'indent-guide 'ample-theme)
-
-;; activate installed packages
-(package-initialize)
-
-;---------------------------------------------------------------------------------------------------------------------;
-; Font
-;;(add-to-list 'default-frame-alist
-;;			 '(font . "InputMono 12"))
-(set-face-attribute 'default nil :family "InputMono" :height 115 :weight 'regular :width 'condensed)
-
-; Tab width
+;;; Tab
 (setq-default tab-width 4)
-(setq-default c-basic-offset 4
-              c-indent-level 4
-              c-brace-imaginary-offset 0
-              c-brace-offset -4
-              c-argdecl-indent 4
-              c-label-offset -4
-              c-continued-statement-offset 4)
+(setq-default tab-stop-list (number-sequence 4 120 4))
+(setq-default indent-tabs-mode nil)
 
-; Disable startup messages
+;;; Disable messaging and splash screen
 (setq-default message-log-max nil)
 (kill-buffer "*Messages*")
-(kill-buffer "*scratch*")
-
-; Disables completions buffer
-(add-hook 'minibuffer-exit-hook
-	  '(lambda ()
-	     (let ((buffer "*Completions*"))
-	       (and (get-buffer buffer)
-		        (kill-buffer buffer)))))
-
-; Disable splash screen
+(setq ring-bell-function 'ignore)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-screen t
       initial-buffer-choice nil)
 
-; Disable bell sounds
-(setq visible-bell 1)
-
-; Enable line numbering
-(global-linum-mode t)
-
-; Disable continued line symbol
-(setf (cdr (assq 'continuation fringe-indicator-alist))
-      '(nil right-curly-arrow))
-
-; Disable backup
+;;; Random customization
+(blink-cursor-mode 0)
+(setq column-number-mode t)
 (setq make-backup-files nil)
-
-; Set username and email ID
-(setq user-full-name "Saurabh Sangpal"
-      user-mail-address "saurabhsangpal@gmail.com")
-
-; Rebinding moving between buffers to C-Right arrow and vice versa
-(defun prev-window()
-  (interactive)
-  (other-window -1))
-(global-set-key "\033[5D" 'prev-window)
-(global-set-key "\033[5C" 'other-window)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit)))
- '(tab-stop-list (quote (4 8 12 16 20))))
-
-; Theme
-(load-theme 'ample t t)
-(enable-theme 'ample)
-
-; Set frame size
-(add-to-list 'default-frame-alist '(height . 40))
-(add-to-list 'default-frame-alist '(width . 125))
-
-; Disable menubar, scrollbar and toolbar
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-
-;; Indent using spaces
-(setq-default indent-tabs-mode t)
-
-;; Disable case sensitive searching
-(setq-default case-fold-search t)
-
-;; Use S-left S-right S-up S-down to switch frames
-(windmove-default-keybindings 'meta)
-
-;; Bind format to C-M-F
-(global-set-key (kbd "C-M-f") 'indent-region)
-
-; Bind C-return to jump to new line w/o breaking current line
-(defun newline-without-breaking-line ()
-  (interactive)
-  (let ((oldpos (point)))
-    (end-of-line)
-    (newline-and-indent)))
-(global-set-key (kbd "<C-return>") 'newline-without-breaking-line)
-
-;; Move line up and down
-(defun move-line-up()
-  (interactive)
-  (transpose-lines 1)
-  (forward-line -2)
-  (indent-according-to-mode))
-(defun move-line-down()
-  (interactive)
-  (forward-line 1)
-  (transpose-lines 1)
-  (forward-line -1)
-  (indent-according-to-mode))
-(global-set-key [(control up)] 'move-line-up)
-(global-set-key [(control down)] 'move-line-down)
-
-(fset 'n
-      [end ?\; return])
-(define-key global-map [f4] 'call-last-kbd-macro)
-
-(defun semicolon-and-new-line()
-  (interactive)
-  (end-of-line)
-  (insert ";")
-  (newline-and-indent))
-(global-set-key (kbd "C-;") 'semicolon-and-new-line)
-
-;; Delete trailing whitespace before saving
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;---------------------------------------------------------------------------------------------------------------------;
-;; Company
-(add-hook 'after-init-hook 'global-company-mode)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
-;; Omnisharp
-(eval-after-load
-  'company
-  '(add-to-list 'company-backends #'company-omnisharp))
+;;; Windows specific customization
+(if (eq system-type 'windows-nt)
+      (menu-bar-mode -1))
 
-(defun my-csharp-mode-setup ()
-  (omnisharp-mode)
-  (company-mode)
-  (flycheck-mode)
+;;; Highlight line
+(global-hl-line-mode 1)
 
-  ;csharp-mode README.md recommends this too
-  ;(electric-pair-mode 1)       ;; Emacs 24
-  ;(electric-pair-local-mode 1) ;; Emacs 25
+;;; Autoreload changed files
+(global-auto-revert-mode t)
 
-  (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
-  (local-set-key (kbd "C-c C-c") 'recompile))
+;;; Username
+(setq user-full-name "Saurabh Sangpal"
+      user-mail-address "saurabh.s@juegostudio.net")
 
-(add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
+;;; Set default frame size
+(add-to-list 'default-frame-alist '(height . 40))
+(add-to-list 'default-frame-alist '(width . 100))
 
-; Flycheck
+;;; Switch to previous window
+(defun prev-window()
+  "Switch to previous window."
+  (interactive)
+  (other-window -1))
+
+(global-set-key (kbd "<s-left>") 'prev-window)
+(global-set-key (kbd "<s-right>") 'other-window)
+
+;;; Package configuration---------------------------------------------
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode)
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2))
+
 (use-package flycheck
   :ensure t
-  :init (global-flycheck-mode))
+  :init
+  (global-flycheck-mode))
 
-; Helm
-(require 'helm-config)
-(global-set-key (kbd "<apps>") 'helm-buffers-list)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(setq helm-M-x-fuzzy-match t)
+(use-package projectile
+  :ensure t
+  :init (projectile-mode +1)
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
 
-;; Projectile
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-move-forward-on-expand        nil
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-user-mode-line-format         nil
+          treemacs-user-header-line-format       nil
+          treemacs-width                         35)
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
 
-; Dash syntax highlighting
-(eval-after-load 'dash '(dash-enable-font-lock))
+(use-package treemacs-evil
+  :after treemacs evil
+  :ensure t)
 
-; Async
-(require 'dired-async)
-(dired-async-mode 1)
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
 
-; Popwin
-(require 'popwin)
-(popwin-mode 1)
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
 
-; Company
-(add-hook 'after-init-hook 'global-company-mode)
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
 
-; Yasnippet
-(require 'yasnippet)
-(yas-global-mode 1)
+(use-package omnisharp
+  :ensure t
+  :config
+  (add-hook 'csharp-mode-hook 'omnisharp-mode)
+  (add-hook 'before-save-hook 'omnisharp-code-format-entire-file)
+  (electric-pair-local-mode 1))
 
-; Smartparens
-(use-package smartparens-config
-  :ensure smartparens
-  :config (progn (show-smartparens-global-mode t)))
-(add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
-(add-hook 'csharp-mode 'turn-on-smartparens-strict-mode)
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-omnisharp 'company-yasnippet))
 
-; Indent guide
-(require 'indent-guide)
-(indent-guide-global-mode)
-(setq indent-guide-delay 0.1)
-(setq indent-guide-char "-")
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1))
 
-;; Powerline
-(require 'powerline)
-(powerline-default-theme)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package popwin
+  :ensure t
+  :init (popwin-mode 1))
+
+(use-package counsel
+  :ensure t
+  :init
+  (ivy-mode 1)
+  :bind
+  ("s-b" . counsel-switch-buffer))
+
+(use-package counsel-projectile
+  :ensure t)
+
+(use-package hl-todo
+  :ensure t
+  :init (global-hl-todo-mode 1)
+  :config
+  (setq hl-todo-keyword-faces
+         '(("BUG" . "#ff9999")
+           ("TODO" . "#00ace6")
+           ("FIXME" . "#ff9900")
+           ("NOTE" . "#66ff66"))))
+
+(use-package indent-guide
+  :ensure t
+  :init (indent-guide-global-mode)
+  :config
+  (setq indent-guide-delay 0.)
+  (setq indent-guide-char "●"))
+
+(use-package smooth-scroll
+  :ensure t
+  :config (smooth-scroll-mode 1))
+
+(use-package smartparens
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'smartparens-mode))
+
+(use-package magit
+  :ensure t)
+
+(use-package git-gutter-fringe
+  :ensure t
+  :config
+  (global-git-gutter-mode))
+
+;;; Themes
+(use-package ample-theme
+  :ensure t
+  :init (progn (load-theme 'ample-flat t t)
+               (enable-theme 'ample-flat))
+  :defer t)
+
+;;; Vim mode helper functions
+(defun scroll-down-5x()
+  "Scrolls down 5 lines."
+  (interactive)
+  (scroll-up-line 5))
+
+(defun scroll-up-5x()
+  "Scrolls up 5 lines."
+  (interactive)
+  (scroll-down-line 5))
+
+;; Vim mode
+(use-package evil
+  :ensure t
+  :init (evil-mode)
+  :config
+  (evil-define-key 'normal 'global (kbd "C-e") 'scroll-down-5x)
+  (evil-define-key 'normal 'global (kbd "C-y") 'scroll-up-5x)
+  (evil-define-key 'normal 'global (kbd "C-o") 'counsel-projectile-find-file)
+  (evil-define-key 'normal 'global (kbd "g h") 'omnisharp-current-type-documentation)
+
+  (evil-set-leader 'nil (kbd ",")))
+
+(use-package evil-escape
+  :ensure t
+  :init (evil-escape-mode)
+  :config
+  (setq-default evil-escape-key-sequence ",,")
+  (setq-default evil-escape-delay 0.2))
+
+(use-package linum-relative
+  :ensure t
+  :init (linum-relative-global-mode)
+  :config (setq linum-relative-backend 'display-line-numbers-mode))
+
+(use-package which-key
+  :ensure t
+  :init (which-key-mode)
+  :config (setq which-key-popup-type 'minibuffer))
+
+(use-package wakatime-mode
+  :ensure t
+  :init
+  (global-wakatime-mode))
+
+(provide 'init)
+;;; Init complete
